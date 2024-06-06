1e934c2df8459a1909df7461ffa80201993cb92d
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-06-06 15:39:57 +0200

    .

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -13,4 +13,8 @@ class Recipe extends Model
     public function category() {
         return $this->belongsTo(Category::class);
     }
+
+    public function ingredients() {
+        return $this->HasMany(Ingredient::class);
+    }
 }
